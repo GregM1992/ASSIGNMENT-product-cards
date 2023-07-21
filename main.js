1a576ad0e8aee496ae94d4dce838075420b3c47d
Gregory Markus <gregmarkus1992@yahoo.com>
2023-07-21 13:11:22 -0500

    rendered sold out dynamically the best i could

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ const spiderInfo = [
   {
     id:1,
     spiderType: "Phiddipus Regius",
-    spiderDescription: "Fuzzy, Cute, Tiny",
+    spiderDescription: "They can have chelicerae that are blue, teal, green, gold, purple, pink, red and orange. Although females can have green and blue which are commonly seen in males, males are not noted to have the pinks and reds",
     availability: true,
     specifications: {
       gender: "unknown",
@@ -38,7 +38,7 @@ const spiderInfo = [
  {
   id:3,
   spiderType: "Phiddipus Arizonensis",
-  spiderDescription: "Fuzzy, Cute, Really tiny, Maybe orange",
+  spiderDescription: "Their cephalothorax is black, with white markings on the sides. The legs are gold-colored, while their abdomen is gray with a black spot near its rear",
   availability: true,
   specifications: {
     gender: "unknown",
@@ -88,25 +88,24 @@ const cardsOnDom = (array) => {
     domString += 
      `
       <div class="card" style="width: 18rem;">
-        <div class="card-body">
-          <div class="top">
-            <h2 class="card-title">${spider.spiderType}</h2>
-            <img src=${spider.availability ? `${spider.imageUrl[0]}` : `${spider.imageUrl[1]}`  } class="card-img-top" alt="...">
-            <p class="card-text">${spider.spiderDescription}</p>
-            <p id="avail" class="card-text">${spider.availability ? 'Available!': 'Sold-Out :('}</p>
-          </div>
-          <div class="middle">
-            <header>Information</header>
-            <p>Gender: ${spider['specifications'].gender}</p>
-            <p>Instar: ${spider['specifications'].instar}</p>
-            <footer>DOB: ${spider['specifications'].birth}</footer>
-          </div>
-          <div class="bottom">
-            <header>Pricing</header>
-            <p>Price for 5:  ${spider['pricing'].one}</p>
-            <p>Price for 10:  ${spider['pricing'].five}</p>
-            <p>Price for 20:  ${spider['pricing'].twenty}</p>
-          </div>
+        <div id="top">
+          <h2 class="spider-type">${spider.spiderType}</h2>
+          <img src=${spider.imageUrl[0]} class="card-img-top" alt="${spider.spiderType}">
+          ${spider.availability ? `<div class="sold-out-img"> <img src="${spider.imageUrl[1]}"> </div>` : ` `}
+          <p class="description">${spider.spiderDescription}</p>
+          <p id="avail" >${spider.availability ? 'Available!': 'Sold-Out :('}</p>
+        </div>
+        <div id="information">
+          <header>Information:</header>
+          <p>Gender: ${spider['specifications'].gender}</p>
+          <p>Instar: ${spider['specifications'].instar}</p>
+          <footer>DOB: ${spider['specifications'].birth}</footer>
+        </div>
+        <div id="pricing">
+          <header>Pricing</header>
+          <p>Price for 5:  ${spider['pricing'].one}</p>
+          <p>Price for 10:  ${spider['pricing'].five}</p>
+          <p>Price for 20:  ${spider['pricing'].twenty}</p>
         </div>
       </div>
       `;
